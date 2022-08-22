4aad5c88299336f84b1a591517a3220a8fad9c3b
hid0 <faizhid11@gmail.com>
2022-08-23 06:50:51 +0700

    create files, next layouting

---
+++ src/components/NotesList.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import getInitialData from "../utils";
+
+export default function NotesList() {
+  return (
+    <>
+      {/* {getInitialData.map((note) => {
+        note.title;
+      })} */}
+    </>
+  );
+}
